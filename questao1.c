eed5f699c6e77f8fb3928a3ca3a9b23fef542bf1
EmellyAndrade <edsa.gen21@uea.edu.br>
2022-08-23 20:36:54 -0300

    atualizei a questão 1

--- questao1.c
+++ questao1.c
@@ -1,4 +1,5 @@
 /*
+Emelly da Silva Andrade - 2115200005
 Herbert Lucas Corrêa Fonseca - 2015090044
 */
 
@@ -6,17 +7,24 @@ Herbert Lucas Corrêa Fonseca - 2015090044
 
 int main (int argc, char **argv)
 {
+
 	int n, maior=-99999, i=0;
+
 	while(i<5)
 	{
+
 		printf("Digite o número:\n");
 		scanf("%d", &n);
+
 		if (n>maior)
 		{
 			maior=n;
 		}
+
 		i++;
 	}
+
 	printf("O maior número:%d\n", maior);
+
 	return 0;
 }
